aef00d667ec5187a4ef99bdb578cec9f983fdd69
winkerVSbecks <varunvachhar@gmail.com>
2024-06-12 11:14:09 -0400

    add viewport test

--- tests/segmented-tooltip.spec.js
+++ tests/segmented-tooltip.spec.js
@@ -13,3 +13,13 @@ test('displays tooltip', async ({ page }) => {
     page.locator('css=#tooltip-1 .tooltip__content-desc.glitch')
   ).toHaveCSS('opacity', '1');
 });
+
+test.use({
+  viewport: { width: 800, height: 900 },
+});
+
+test('meta is hidden on smaller screens', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8080');
+  for (const meta of await page.locator('css=.meta').all())
+    await await expect(meta).toBeHidden;
+});
